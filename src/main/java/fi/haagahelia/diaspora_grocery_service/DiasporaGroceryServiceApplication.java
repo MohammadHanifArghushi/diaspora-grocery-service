b3679effa7c095fc2abdf2458a2a34ae9e601019
MohammadHanifArghushi <mha.lafg@gmail.com>
2025-10-01 17:35:41 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/fi/haagahelia/diaspora_grocery_service/DiasporaGroceryServiceApplication.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.diaspora_grocery_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiasporaGroceryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiasporaGroceryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/haagahelia/diaspora_grocery_service/DiasporaGroceryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.diaspora_grocery_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiasporaGroceryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
